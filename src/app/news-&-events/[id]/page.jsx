4de1a3f9e49c8b1cb16aa311eb472053bff89584
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-16 23:20:52 +0500

    responsive fixes

--- src/app/news-&-events/[id]/page.jsx
+++ src/app/news-&-events/[id]/page.jsx
@@ -1,13 +1,13 @@
 import { getNewsAndEvents, getSingleNewsAndEvents } from '@/services'
 import NewsAndEventDetailsTemplate from '@/templates/NewsAndEventDetails'
 
-const NewsAndEventDetails = async ({params}) => {
+const NewsAndEventDetails = async ({ params }) => {
     const data = await getNewsAndEvents()
     const content = await getSingleNewsAndEvents(params?.id)
 
     return (
         <>
-            <NewsAndEventDetailsTemplate data={data?.data} content={content?.[0]}/>
+            <NewsAndEventDetailsTemplate data={data?.data} content={content?.[0]} />
         </>
     )
 }
