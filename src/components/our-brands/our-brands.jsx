55beaed177c1cd73c76337a1ebee029f7898bfc6
Muhammad Shoaib <m.xhoaib1@gmail.com>
2025-01-20 13:00:05 +0500

    we all brands linking fixed

--- src/components/our-brands/our-brands.jsx
+++ src/components/our-brands/our-brands.jsx
@@ -90,7 +90,7 @@ const OurBrands = ({ data, content }) => {
             <div className="w-10 h-[1px] bg-theme-main" />
           </h3>
           <div className="flex items-center gap-7">
-            <Link href="/about" className="font_calibri hidden md:block text-nowrap font-light text-white leading-[139%] py-[10px] px-[28px] bg-[#2e368fbf] hover:bg-theme-main rounded-[10px]">View All Brands</Link>
+            <Link href="/distributors-&-stockists" className="font_calibri hidden md:block text-nowrap font-light text-white leading-[139%] py-[10px] px-[28px] bg-[#2e368fbf] hover:bg-theme-main rounded-[10px]">View All Brands</Link>
             <div className="items-center gap-2 hidden md:flex">
               <Image
                 src="/svg/arrow_circle_left.svg"
@@ -158,7 +158,7 @@ const OurBrands = ({ data, content }) => {
             onClick={() => slider?.current?.slickNext()}
           />
         </div>
-        <Link href="/about" className="font_calibri md:hidden  text-nowrap bg-[#2e368fbf] hover:bg-theme-main font-light text-white leading-[139%] py-[10px] px-[28px] rounded-[10px]">View All Brands</Link>
+        <Link href="/distributors-&-stockists" className="font_calibri md:hidden  text-nowrap bg-[#2e368fbf] hover:bg-theme-main font-light text-white leading-[139%] py-[10px] px-[28px] rounded-[10px]">View All Brands</Link>
       </div>
     </section>
   );
